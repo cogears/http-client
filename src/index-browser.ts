0c70f3ae1cfc7cce7f290ad0b6c620c29e2e4531
stone <>
2025-06-07 18:05:28 +0800

    fix bug

--- src/index-browser.ts
+++ src/index-browser.ts
@@ -3,6 +3,8 @@ import { HttpBody } from './HttpBody.js'
 import HttpClient, { HttpOptions, HttpResponse } from './HttpClient.js'
 import HttpAjaxClient from './impl/HttpAjaxClient.js'
 export { HttpApi, HttpBody, HttpClient, HttpOptions, HttpResponse }
+
+const httpInstance = new HttpAjaxClient()
 export default function (): HttpClient {
-    return new HttpAjaxClient()
+    return httpInstance
 }
--- src/index-node.ts
+++ src/index-node.ts
@@ -3,6 +3,8 @@ import { HttpBody } from './HttpBody.js'
 import HttpClient, { HttpOptions, HttpResponse } from './HttpClient.js'
 import HttpNodeClient from './impl/HttpNodeClient.js'
 export { HttpApi, HttpBody, HttpClient, HttpOptions, HttpResponse }
+
+const httpInstance = new HttpNodeClient()
 export default function (): HttpClient {
-    return new HttpNodeClient()
+    return httpInstance
 }
